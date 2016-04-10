44b18a0205e1d181a1f0506f818eb47a275b6ddf
Alexey Nesterov <zix988@gmail.com>
2016-04-10 11:13:49 +0600

    Code style.

--- app/src/main/java/com/trendtechnology/notes/utils/DateUtils.java
+++ app/src/main/java/com/trendtechnology/notes/utils/DateUtils.java
@@ -31,25 +31,24 @@ public class DateUtils {
      * Возвращает короткую запись даты или времени в зависимости от разницы времени параметра и
      * настоящего времени.
      *
-     * @param date - Дата прошлого события. Например дата создания заметки.
+     * @param displayDate - Дата которую необходимо привести к короткой форме.
      * @return - короткую строку с датой. Например: "3:35" | "7 апр." | "2015"
      */
-    public static String getShortDateDiff(Date date) {
-        return getShortDateDiff(date, new Date());
+    public static String getShortDateDiff(Date displayDate) {
+        return getShortDateDiff(displayDate, new Date());
     }
 
     /**
      * Возвращает короткую запись даты или времени в зависимости от разницы времени в параметрах.
-     * Если разница во времени менее суток приводит pastDate к формату короткой записи в часах.
      *
-     * @param pastDate - Дата прошлого события. Например дата создания заметки.
+     * @param displayDate - Дата которую необходимо привести к короткой форме.
      * @param currentDate - Текущая дата.
      * @return - короткую строку с датой. Например: "3:35" | "7 апр." | "2015"
      */
-    public static String getShortDateDiff(Date pastDate, Date currentDate) {
+    public static String getShortDateDiff(Date displayDate, Date currentDate) {
         // TODO: Дописать вывод записи в часх и годах.
         SimpleDateFormat shortDateFormat = new SimpleDateFormat( "d LLL", Locale.getDefault() );
-        return shortDateFormat.format(pastDate);
+        return shortDateFormat.format(displayDate);
     }
 
 }
