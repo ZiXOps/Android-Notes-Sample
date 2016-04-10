bf35de18b096a378d1a1f643f0dfd7a1412b048f
Alexey Nesterov <zix988@gmail.com>
2016-04-10 18:25:47 +0600

    Added styles for add note activity.

---
+++ app/src/main/java/com/trendtechnology/notes/NoteActivity.java
@@ -0,0 +1,20 @@
+package com.trendtechnology.notes;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Детальный просмотр заметки.
+ *
+ * @author Alexey Nesterov
+ * @version 1.00 8 Apr 2016
+ */
+public class NoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
